b14c260296dcbd1c483d63bd4442b453d858de4d
Nitinegoro <pkpvicky@gmail.com>
2017-10-12 09:02:07 +0700

    kiamat

--- application/views/main-index.php
+++ application/views/main-index.php
@@ -8,6 +8,7 @@
 	<link rel="shortcut icon" type="image/png" href="<?php echo base_url('assets/public/image/favicon-title.png') ?>"/>
 	<link rel="stylesheet" href="<?php echo base_url("assets/public/bootstrap/css/bootstrap.min.css"); ?>">
 	<link rel="stylesheet" href="<?php echo base_url("assets/skpd/css/main.css"); ?>">
+	<link rel="stylesheet" href="<?php echo base_url("assets/public/font-awesome/css/font-awesome.min.css"); ?>">
 </head>
 <body>
 	<nav class="navbar navbar-default" role="navigation">
@@ -16,6 +17,45 @@
                 <img src="<?php echo base_url("assets/public/image/logo-main.png"); ?>" class="logo-head" alt="Logo" width="440">
 			</div>
 		</div>
+	<div class="line">
+		<div class="box-line-orange">
+			<span class="text-line">Motto : "Selawang Segantang"</span>
+		</div>
+	</div>
 	</nav>
+	<div class="container">
+		<div class="row centered">
+			<div class="col-md-2 col-xs-4 button">
+				<a href="" class="button-bigger">
+					<i class="fa fa-phone"></i>
+					<h2 class="text-button">Kontak</h2>
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-4 button">
+				<a href="" class="button-bigger">
+					<i class="fa fa-users"></i>
+					<h2 class="text-button">PUBLIK</h2>
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-4 button">
+				<a href="" class="button-bigger">
+					<i class="fa fa-info-circle"></i>
+					<h2 class="text-button">Informasi</h2>
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-4 button">
+				<a href="" class="button-bigger">
+					<i class="fa fa-sign-in"></i>
+					<h2 class="text-button">Login</h2>
+				</a>
+			</div>
+		</div>
+	</div>
+<div class="navbar navbar-inverse navbar-fixed-bottom">
+   <div class="container">
+      <small class="navbar-text pull-left">&copy; SEKRETARIAT DAERAH KABUPATEN BANGKA TENGAH 2017</small>
+      <small class="navbar-text pull-right">Develop By <a href="" target="_blank">Teitra Mega</a></small>
+   </div>
+</div>	
 </body>
 </html>
